0c63ae6411bc1882c150becee71614b79c9f97ac
Pranav Prasad <pranavpd7@gmail.com>
2025-06-21 16:00:56 -0700

    front-end for authentication

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,18 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function LoginPage() {
+export default function SignUpPage() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white px-6">
       <div className="w-full max-w-xs space-y-6">
@@ -37,17 +25,26 @@ export default function LoginPage() {
             />
           </div>
 
+          <div>
+            <label className="block text-sm text-gray-700 mb-1">Confirm Password:</label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-black placeholder-gray-400 text-black"
+            />
+          </div>
+
           <button
             type="submit"
             className="w-full py-2 mt-2 bg-yellow-400 text-black rounded-full text-sm font-semibold"
           >
-            Log In
+            Sign Up
           </button>
 
           <p className="text-sm text-center text-gray-600 mt-2">
-            Don’t have an account?{" "}
-            <Link href="/signup" className="text-blue-600 underline">
-              Sign Up
+            Already have an account?{" "}
+            <Link href="/" className="text-blue-600 underline">
+              Log In
             </Link>
           </p>
         </form>
