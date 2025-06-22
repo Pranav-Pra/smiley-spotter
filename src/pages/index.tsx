9dbdc58cfa5226b1ac455c00e5b9cc21111b662e
Pranav Prasad <pranavpd7@gmail.com>
2025-06-21 17:40:00 -0700

    fixed merge conflict with logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
             width={64}
             height={64}
           />
-          <h1 className="text-3xl font-quicksand text-center">SmileSpotter</h1>
+          <h1 className="text-3xl font-quicksand text-center text-black">SmileSpotter</h1>
         </div>
 
         <form onSubmit={handleLogin} className="space-y-4">
